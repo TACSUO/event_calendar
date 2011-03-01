2a5501b118d91b38cc62dcd0abf205d148c10188
Jeremiah Heller <jeremiah@inertialbit.net>
2011-03-01 12:39:09 -0800

    update event lists to one liners, acct for path variations between current & deleted events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,14 @@
 module EventsHelper
+  def event_one_liner(event)
+    path = event.deleted? ? 'event_revision_path' : 'event_path'
+    "#{h(event.name)} #{event_abbrev_date(event)} <span class=\"fake_button\">#{link_to('details', send(path, event))}</span>".html_safe
+  end
+  
+  def event_abbrev_date(event)
+    if event.one_day?
+      "(<em>#{event.start_on.strftime("%a")} #{event.start_day.ordinalize}</em>)"
+    else
+      "(<em>#{event.start_on.strftime("%a")} #{event.start_day.ordinalize} - #{event.end_on.strftime("%a")} #{event.end_day.ordinalize}</em>)"
+    end
+  end
 end
