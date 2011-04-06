11ff08e7530c2b6a42a0446fdfd297fa3aed241f
Jeremiah Heller <jeremiah@inertialbit.net>
2011-04-06 14:14:35 -0700

    display event_type as 'category_label' in events list

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,12 @@
 module EventsHelper
   def event_one_liner(event)
-    "#{h(event.name)} #{event_abbrev_date(event)} #{event_details_link(event)}".html_safe
+    [
+      h(event.name),
+      event_abbrev_date(event),
+      event_type_label(event),
+      event_details_link(event)
+    ].
+    join(" ").html_safe
   end
   
   def event_abbrev_date(event)
@@ -13,6 +19,12 @@ module EventsHelper
   
   def event_details_link(event)
     path = event.deleted? ? 'event_revision_path' : 'event_path'
-    "<span class=\"fake_button\">#{link_to('details', send(path, event))}</span>".html_safe
+    "<span class=\"fake_button\">#{link_to('Details', send(path, event))}</span>".html_safe
+  end
+  
+  def event_type_label(event)
+    event_type_css_class = event.event_type.parameterize('_').downcase +
+                           "_category_label"
+    "<span class=\"category_label #{h(event_type_css_class)}\">#{h(event.event_type)}</span>".html_safe
   end
 end
